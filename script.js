c3058aa721f9b82293c7bae6643fd86392b9d8fc
afroja0099 <fatemaahmed3688@gmail.com>
2024-12-30 16:38:49 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+
+// const sum = 9 + 99;
+// console.log(sum)
+
+// if (sum > 50) {
+//     console.log('Right')
+// }
+// else {
+//     console.log('Wrong')
+// }
+// function names(name) {
+//     console.log(`${name} ,Aysha, Sompa, Khadija`)
+// }
+// names('Salma')
+
+// var a = 0.5;
+// var b = 0.3;
+// var total = a * b;
+// console.log(total.toFixed(2))
+
+
+// const potato = 30;
+
+// if (potato <= 30) {
+//     console.log('give me a potato');
+// }
+// else {
+//     console.log('more than money')
+// }
+
+
+
+// const price = 2200;
+// const weight = 50;
+// const isHappy = true;
+// if ((price > 2000 || weight > 40) && isHappy == true) {
+//     console.log('yes')
+// }
+// else {
+//     console.log('no')
+// }
+
+// const addToCart=400;
+// if(addToCart>=500){
+//     const discount=addToCart*10/100;
+//     const pay=addToCart-discount;
+//     console.log(pay)
+// }
+// else if(addToCart>=250){
+//     const discount=addToCart *5 / 100;
+//     const pay=addToCart-discount
+//     console.log(pay)
+// }
+// else{
+//     console.log(addToCart);
+// }
+
+
+const baby= 6;
+const oldMan=50;
+const isIdCard=true;
+const coponCard=true;
+const buy=550;
+
+if(baby<=5 ||(oldMan>=60 && isIdCard == true)){
+    console.log('free free')
+}
+else if(coponCard==true){
+    const discount=buy*10/100;
+    const pay =buy-discount;
+    console.log(pay)
+}
+else{
+    console.log('pay your food bill')
+}
+
+let price= 400;
+const isBoss=false;
+const pay =isBoss==true? price=0 :price+100;
+console.log(pay)
---
+++ variable.js
@@ -0,0 +1,3 @@
+  var names='salma, aysha, sadhona';
+  names='salma,aysha,sodhona,sompa'
+  console.log(names);
